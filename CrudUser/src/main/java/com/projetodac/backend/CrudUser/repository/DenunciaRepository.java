e1421c01fc50e8fc05374b0572df26e6ba91b59b
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-10-29 15:20:04 -0300

    Delete CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository directory

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/DenunciaRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.projetodac.backend.CrudUser.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetodac.backend.CrudUser.model.Denuncia;
-
-public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
-    
-}
