6bfe4479bacc8a502ac13e9fe1c5617061b13427
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-06 15:46:43 -0300

    Refactor Aluno class by removing unnecessary annotations

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Aluno.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Aluno.java
@@ -2,16 +2,12 @@ package com.projeto.IForum.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "alunos")
-@PrimaryKeyJoinColumn(name = "user_id")
 @Data
 @NoArgsConstructor 
 @AllArgsConstructor
