0064c7b2e54481e125c7f71c0038a6bc25c9d732
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-06 15:48:57 -0300

    Refactor Funcionario class definition

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Funcionario.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Funcionario.java
@@ -2,10 +2,8 @@ package com.projeto.IForum.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "funcionarios")
 public class Funcionario extends Usuario {
 
     @Column(nullable = false, length = 120)
