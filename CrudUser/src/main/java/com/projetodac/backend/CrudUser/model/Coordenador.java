9350b2aa2542cfe08d4ff60dc1bf592d2ef325bc
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-06 15:47:24 -0300

    Refactor Coordenador class for package structure

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Coordenador.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Coordenador.java
@@ -3,10 +3,8 @@ package com.projeto.IForum.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "coordenadores")
 @PrimaryKeyJoinColumn(name = "user_id")
 public class Coordenador extends Usuario {
     
