51df22bcc2a05b0ecf88f3ae626ce60a025de77b
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-06 15:48:18 -0300

    Refactor Denuncia class structure and imports

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Denuncia.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Denuncia.java
@@ -11,10 +11,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "denuncias")
 public class Denuncia {
 
     @Id
