df9146f8aae33336990d1df3a773804b48558261
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-18 01:56:58 -0300

    Implement CRUD operations in FuncionarioController
    Refactored FuncionarioController to manage a list of Funcionarios with CRUD operations.

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/controller/FuncionarioController.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/controller/FuncionarioController.java
@@ -8,6 +8,34 @@ import com.projeto.IForum.service.FuncionarioService;
 
 @RestController
 @RequestMapping("/funcionarios")
-public class FuncionarioController extends UsuarioController<FuncionarioDTO, Long> {
-    private final FuncionarioService funcionarioService;
+public class FuncionarioController{
+    private List<Funcionario> funcionarios = new ArrayList<>();
+
+    @GetMapping("/funcionarios")
+    public List<Funcionario> getAllFuncionarios() {
+        return funcionarios;
+    }
+
+    @PostMapping("/funcionarios")
+    public String addFuncionario(@RequestBody Funcionario funcionario) {
+        funcionario.add(funcionario);
+        return "Funcionário inserido com sucesso";
+    }
+
+    @PutMapping("/funcionarios/{id}")
+    public String updateFuncionario(@PathVariable int id, @RequestBody Funcionario updatedFuncionario) {
+        for (Funcionario funcionarios : funcionarios) {
+            if (funcionarios.getId() == id) {
+                funcionarios.setName(updatedfuncionarios.getName());
+                return "Funcionário atualizado com sucesso";
+            }
+        }
+        return "Funcionário não encontrado!";
+    }
+
+    @DeleteMapping("/funcionarios/{id}")
+    public String deletefuncionarios(@PathVariable int id) {
+        funcionarios.removeIf(funcionarios -> funcionarios.getId() == id);
+        return "Funcionário deletado com sucesso";
+    }
 }
