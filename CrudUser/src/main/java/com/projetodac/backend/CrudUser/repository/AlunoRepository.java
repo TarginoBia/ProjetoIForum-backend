f9a3e3ed8090de239ce5bcdc628e2ec1777264ca
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-18 02:00:49 -0300

    Implement AlunoRepository with CRUD operations

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/AlunoRepository.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/AlunoRepository.java
@@ -1,5 +1,20 @@
 package com.projeto.IForum.repository;
 
-public class AlunoRepository {
-    
+public class AlunoRepository implements UsuarioRepository<Aluno>{
+
+    private Map<Long, Aluno> repository;
+
+    public AlunoRepository() {
+        this.repository = new HashMap<>();
+    }
+
+    @Override
+    public void save(Aluno aluno) {
+        repository.put(aluno.getId(), aluno);
+    }
+
+    @Override
+    public Aluno findAlunoById(Long id) {
+        return repository.get(id);
+    }
 }
