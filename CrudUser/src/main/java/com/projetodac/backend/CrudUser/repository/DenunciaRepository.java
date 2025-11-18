03548d8d40b85f461f5b14b5aa24cd63e64ceaa0
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-18 02:01:44 -0300

    Implement DenunciaRepository with CRUD methods

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/DenunciaRepository.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/DenunciaRepository.java
@@ -1,5 +1,20 @@
 package com.projeto.IForum.repository;
 
-public class DenunciaRepository {
+public class DenunciaRepository{
     
+    private Map<Long, Denuncia> repository;
+
+    public DenunciaRepository() {
+        this.repository = new HashMap<>();
+    }
+
+    @Override
+    public void save(Denuncia denuncia) {
+        repository.put(denuncia.getId(), denuncia);
+    }
+
+    @Override
+    public Denuncia findDenunciaById(Long id) {
+        return repository.get(id);
+    }
 }
