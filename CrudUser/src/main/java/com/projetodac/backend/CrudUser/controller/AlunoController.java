7898e8809aa916b35baa5716e9b71e320b2e2a7c
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-18 01:55:12 -0300

    Implement CRUD operations in AlunoController
    Refactored AlunoController to manage a list of Aluno objects with CRUD operations.

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/controller/AlunoController.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/controller/AlunoController.java
@@ -8,6 +8,34 @@ import com.projeto.IForum.service.AlunoService;
 
 @RestController
 @RequestMapping("/alunos")
-public class AlunoController extends UsuarioController<AlunoDTO, Long> {
-    private final AlunoService alunoService; 
+public class AlunoController{
+    private List<Aluno> alunos = new ArrayList<>();
+
+    @GetMapping("/alunos")
+    public List<Aluno> getAllAlunos() {
+        return alunos;
+    }
+
+    @PostMapping("/alunos")
+    public String addAluno(@RequestBody Aluno alunos) {
+        alunos.add(alunos);
+        return "Aluno inserido com sucesso";
+    }
+
+    @PutMapping("/alunos/{id}")
+    public String updateAluno(@PathVariable int id, @RequestBody Aluno updatedAluno) {
+        for (Aluno alunos : alunos) {
+            if (alunos.getId() == id) {
+                alunos.setName(updatedAlunos.getName());
+                return "Aluno atualizado com sucesso";
+            }
+        }
+        return "Aluno não encontrado!";
+    }
+
+    @DeleteMapping("/alunos/{id}")
+    public String deleteAlunos(@PathVariable int id) {
+        alunos.removeIf(alunos -> alunos.getId() == id);
+        return "Aluno deletado com sucesso";
+    }
 }
