b76206a2495a4a4b57a4069b0c436940a5d1ced1
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-18 02:02:07 -0300

    Implement FuncionarioRepository with CRUD methods

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/FuncionarioRepository.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/FuncionarioRepository.java
@@ -1,5 +1,20 @@
 package com.projeto.IForum.repository;
 
-public class FuncionarioRepository {
+public class FuncionarioRepository implements UsuarioRepository{
     
+    private Map<Long, Funcionario> repository;
+
+    public FuncionarioRepository() {
+        this.repository = new HashMap<>();
+    }
+
+    @Override
+    public void save(Funcionario funcionario) {
+        repository.put(funcionario.getId(), funcionario);
+    }
+
+    @Override
+    public Funcionario findFuncionarioById(Long id) {
+        return repository.get(id);
+    }
 }
