b8661ba692c2c5b28eaa61f1a3029f11dcbb4212
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-18 02:01:16 -0300

    Implement CoordenadorRepository with CRUD methods

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/CoordenadorRepository.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/CoordenadorRepository.java
@@ -1,5 +1,20 @@
 package com.projeto.IForum.repository;
 
-public class CoordenadorRepository {
+public class CoordenadorRepository implements UsuarioRepository<Coordenador>{
     
+    private Map<Long, Coordenador> repository;
+
+    public CoordenadorRepository() {
+        this.repository = new HashMap<>();
+    }
+
+    @Override
+    public void save(Coordenador coordenador) {
+        repository.put(coordenador.getId(), coordenador);
+    }
+
+    @Override
+    public Coordenador findCoordenadorById(Long id) {
+        return repository.get(id);
+    }
 }
