ae9956c08519863a071175700d353993ec80fc6a
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-11-20 02:05:17 -0300

    Refactor FuncionarioRepository to extend JpaRepository

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/FuncionarioRepository.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/repository/FuncionarioRepository.java
@@ -1,20 +1,9 @@
 package com.projeto.IForum.repository;
 
-public class FuncionarioRepository implements UsuarioRepository{
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.IForum.model.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario,Long>{
     
-    private Map<Long, Funcionario> repository;
-
-    public FuncionarioRepository() {
-        this.repository = new HashMap<>();
-    }
-
-    @Override
-    public void save(Funcionario funcionario) {
-        repository.put(funcionario.getId(), funcionario);
-    }
-
-    @Override
-    public Funcionario findFuncionarioById(Long id) {
-        return repository.get(id);
-    }
 }
