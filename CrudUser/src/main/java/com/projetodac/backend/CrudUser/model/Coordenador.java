c594697bf5134acd325c9d753ad6dedd1a3e492d
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-12-02 17:27:09 -0300

    Update Coordenador.java

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Coordenador.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Coordenador.java
@@ -3,9 +3,17 @@ package com.projeto.IForum.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-@Entity
 @PrimaryKeyJoinColumn(name = "user_id")
+@Entity
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class Coordenador extends Usuario {
     
     @Column(nullable = false, length = 120)
