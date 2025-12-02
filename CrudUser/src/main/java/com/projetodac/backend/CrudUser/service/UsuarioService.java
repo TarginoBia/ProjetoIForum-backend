da97eaa9189a0ea7363fedbd90929ecfe00a8b32
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-12-02 17:38:12 -0300

    Update UsuarioService.java

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/service/UsuarioService.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/service/UsuarioService.java
@@ -3,7 +3,6 @@ package com.projeto.IForum.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projeto.IForum.model.Usuario;
@@ -14,7 +13,6 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
-    @Autowired 
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
