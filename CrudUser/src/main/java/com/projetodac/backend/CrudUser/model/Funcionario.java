46c18052c3ad6300561b80da3aea4b50e43f20e7
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-12-02 17:29:18 -0300

    Enhance Funcionario class with new fields and annotations
    Added departamento field and Lombok annotations.

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Funcionario.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/model/Funcionario.java
@@ -2,10 +2,21 @@ package com.projeto.IForum.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class Funcionario extends Usuario {
 
     @Column(nullable = false, length = 120)
     private String cargo;
+
+    @Column(nullable = false, length = 120)
+    private String departamento;
 }
