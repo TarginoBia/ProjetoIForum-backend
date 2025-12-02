5c72abbc1b6b392bfc6ab7ea6e97d162e8f53e72
Maria Beatriz Targino <112200332+TarginoBia@users.noreply.github.com>
2025-12-02 17:35:33 -0300

    Update DenunciaService.java

--- CrudUser/src/main/java/com/projetodac/backend/CrudUser/service/DenunciaService.java
+++ CrudUser/src/main/java/com/projetodac/backend/CrudUser/service/DenunciaService.java
@@ -2,10 +2,12 @@ package com.projeto.IForum.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.projeto.IForum.dto.DenunciaDTO;
 import com.projeto.IForum.model.Denuncia;
 import com.projeto.IForum.repository.DenunciaRepository;
 
@@ -13,25 +15,57 @@ import com.projeto.IForum.repository.DenunciaRepository;
 public class DenunciaService{
 
     private final DenunciaRepository denunciaRepository;
-
-    @Autowired 
+ 
     public DenunciaService(DenunciaRepository denunciaRepository) {
         this.denunciaRepository = denunciaRepository;
     }
 
-    public Denuncia salvarDenuncia(Denuncia denuncia) {
-        return denunciaRepository.save(denuncia);
-    }
-
-    public Optional<Denuncia> buscarPorId(Long id) {
-        return denunciaRepository.findById(id);
-    }
-
-    public List<Denuncia> buscarTodos() {
-        return denunciaRepository.findAll();
+    @Transactional
+    public DenunciaDTO salvar(DenunciaDTO dto) {
+        Denuncia denuncia = DenunciaDTO.toEntity(dto);
+        Denuncia denunciaSalvo = denunciaRepository.save(denuncia);
+        return DenunciaDTO.fromEntity(denunciaSalvo);
     }
     
-    public void deletarDenuncia(Long id) {
+    @Transactional(readOnly = true)
+    public List<DenunciaDTO> buscarTodos() {
+        return denunciaRepository.findAll().stream()
+                     .map(DenunciaDTO::fromEntity)
+                     .collect(Collectors.toList());
+    }
+    
+    @Transactional(readOnly = true)
+    public Optional<DenunciaDTO> buscarPorId(Long id) {
+        return denunciaRepository.findById(id)
+            .map(DenunciaDTO::fromEntity); 
+    }
+
+    @Transactional
+    public Optional<DenunciaDTO> atualizar(DenunciaDTO dto) {
+        
+        Optional<Denuncia> optionalDenuncia = denunciaRepository.findById(dto.getId());
+
+        if (optionalDenuncia.isEmpty()) {
+            return Optional.empty();    
+        }
+
+        Denuncia denunciaExistente = optionalDenuncia.get();
+        
+            denunciaExistente.setDescricao(dto.getDescricao());
+            denunciaExistente.setTitulo(dto.getTitulo());
+            denunciaExistente.setStatus(dto.getStatus());
+        
+        Denuncia denunciaAtualizado = denunciaRepository.save(denunciaExistente);
+        
+        return Optional.of(DenunciaDTO.fromEntity(denunciaAtualizado));
+    }
+
+    @Transactional
+    public boolean deletar(Long id) {
+        if (!denunciaRepository.existsById(id)) {
+             return false; 
+        }
         denunciaRepository.deleteById(id);
+        return true;
     }
 }
